0bec411aa687399b4997ee833b316d657e89f62b
CresixU <40704964+CresixU@users.noreply.github.com>
2021-05-23 13:39:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,102 +8,75 @@ import cv2
 import os
 import numpy as np
 
-labels = ['healthy', 'rotten']
-img_size = 32
-def get_data(data_dir):
-    data = []
-    for label in labels:
-        path = os.path.join(data_dir, label)
-        class_num = labels.index(label)
-        for img in os.listdir(path):
-            try:
-                img_arr = cv2.imread(os.path.join(path, img))[...,::-1]
-                resized_arr = cv2.resize(img_arr, (img_size, img_size))
-                data.append([resized_arr, class_num])
-            except Exception as e:
-                print(e)
-    return np.array(data)
-
-train = get_data('data/')
-val = get_data('test/')
-
-x_train = []
-y_train = []
-x_val = []
-y_val = []
-
-for feature, label in train:
-  x_train.append(feature)
-  y_train.append(label)
-
-for feature, label in val:
-  x_val.append(feature)
-  y_val.append(label)
-
-x_train = np.array(x_train) / 255
-x_val = np.array(x_val) / 255
-x_train.reshape(-1, img_size, img_size, 1)
-y_train = np.array(y_train)
-x_val.reshape(-1, img_size, img_size, 1)
-y_val = np.array(y_val)
-
-datagen = ImageDataGenerator(
-        featurewise_center=False,
-        samplewise_center=False,
-        featurewise_std_normalization=False,
-        samplewise_std_normalization=False,
-        zca_whitening=False,
-        rotation_range = 180,
-        zoom_range = 0.2,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        horizontal_flip = True,
-        vertical_flip=True)
+train_datagen = ImageDataGenerator(
+    rescale=1/255,
+    rotation_range = 40,
+    zoom_range = 0.2,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    horizontal_flip = True,
+    vertical_flip=True,
+    )
+test_datagen = ImageDataGenerator(
+    rescale=1/255
+)
+img_size = 48
 
 
-datagen.fit(x_train)
+train_generator = train_datagen.flow_from_directory(
+    'data/train',
+    target_size=(img_size, img_size),
+    batch_size=4,
+    shuffle=False,
+    seed=3,
+    class_mode='categorical',
+    color_mode='rgb',
+    save_to_dir='data/tmp'
+)
+print(train_generator.labels)
+print(type(train_generator))
+
+test_generator = test_datagen.flow_from_directory(
+    'data/test',
+    target_size=(img_size, img_size),
+    batch_size=4,
+    shuffle=False,
+    seed=3,
+    class_mode='categorical',
+    color_mode='rgb'
+)
+
+x,y = test_generator.next()
 
 model = Sequential()
-model.add(Conv2D(32,3, activation="relu", input_shape=(img_size,img_size,3)))
-model.add(MaxPool2D(2,2))
+model.add(Conv2D(32, 3, activation="relu", input_shape=(img_size, img_size, 3)))
+model.add(MaxPool2D(2, 2))
 model.add(Conv2D(32, 3, activation="relu"))
-model.add(MaxPool2D(2,2))
+model.add(MaxPool2D(2, 2))
 model.add(Conv2D(64, 3, activation="relu"))
-model.add(MaxPool2D(2,2))
+model.add(MaxPool2D(2, 2))
 model.add(Dropout(0.4))
 model.add(Flatten())
-model.add(Dense(128,activation="relu"))
+model.add(Dense(128, activation="relu"))
 model.add(Dense(2, activation="softmax"))
 
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=10,
+    epochs=50,
+    validation_data=test_generator,
+    validation_steps=10
+)
 model.summary()
 
+results = model.evaluate(x,y, batch_size=1)
+print("results:",results)
 
-model.compile(optimizer = 'adam' , loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
+predict = model.predict(x[0].reshape(1,img_size,img_size,3))
+print("prediction:",predict)
 
-history = model.fit(x_train,y_train,epochs = 10 , validation_data = (x_val, y_val))
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs_range = range(10)
-
-plt.figure(figsize=(15, 15))
-plt.subplot(2, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
-
-predictions = model.predict_classes(x_val)
-predictions = predictions.reshape(1,-1)[0]
-print(classification_report(y_val, predictions, target_names = ['Healthy','Rotten']))
-
+#model.save("model.h5")
+#tf.saved_model.save(model, "saved-model/1/")
