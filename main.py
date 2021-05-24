30ad20aaf95166eb009406d7aacc00ae5de34e53
CresixU <40704964+CresixU@users.noreply.github.com>
2021-05-24 10:45:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification_report
 import tensorflow as tf
@@ -20,18 +20,19 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(
     rescale=1/255
 )
-img_size = 48
+img_size = 64
+batch_size = 4
 
 
 train_generator = train_datagen.flow_from_directory(
     'data/train',
     target_size=(img_size, img_size),
-    batch_size=4,
-    shuffle=False,
+    batch_size=batch_size,
+    shuffle=True,
     seed=3,
     class_mode='categorical',
-    color_mode='rgb',
-    save_to_dir='data/tmp'
+    color_mode='rgb'
+    #save_to_dir='data/tmp'
 )
 print(train_generator.labels)
 print(type(train_generator))
@@ -39,13 +40,14 @@ print(type(train_generator))
 test_generator = test_datagen.flow_from_directory(
     'data/test',
     target_size=(img_size, img_size),
-    batch_size=4,
-    shuffle=False,
+    batch_size=batch_size,
+    shuffle=True,
     seed=3,
     class_mode='categorical',
     color_mode='rgb'
 )
-
+print(train_generator.samples)
+print(test_generator.samples)
 x,y = test_generator.next()
 
 model = Sequential()
@@ -65,10 +67,10 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 
 history = model.fit(
     train_generator,
-    steps_per_epoch=10,
+    steps_per_epoch=train_generator.samples // batch_size,
     epochs=50,
     validation_data=test_generator,
-    validation_steps=10
+    validation_steps=test_generator.samples // batch_size
 )
 model.summary()
 
@@ -80,3 +82,28 @@ print("prediction:",predict)
 
 #model.save("model.h5")
 #tf.saved_model.save(model, "saved-model/1/")
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
