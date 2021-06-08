e8a74c1d0bc57ee2339dad789d5a4d5dfb367d0a
CresixU <40704964+CresixU@users.noreply.github.com>
2021-06-08 12:17:21 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,14 +1,11 @@
-import cv2
 import numpy as np
 import cv2 as cv
 import os
 from keras import models
-from PIL import Image
 import tensorflow as tf
-from imageai.Detection import VideoObjectDetection
 
 cam_source = 'http://192.168.18.6:4747/video'
-
+labels = ['healthy', 'rotten']
 path = os.getcwd()
 cam = cv.VideoCapture(cam_source)
 
@@ -21,46 +18,18 @@ while True:
     if not ret:
         print("Can't receive stream...")
         break
-    im = Image.fromarray(frame, 'RGB')
-    im = im.resize((64,64))
-    img_array = np.array(im)
-    img_array = np.expand_dims(img_array, axis=0)
-    prediction = model.predict(img_array)[0][0]
+    image = cv.resize(frame, (64, 64)) 
+    image = image[:, :, [2, 1, 0]] 
+    image = image.reshape(1, 64, 64, 3)
+    image = image.astype('float32')
+    image = image/255
+    prediction = model.predict(image)[0][[0]]
+    prediction_class = model.predict(image).argmax(axis=-1)
     if prediction < 0.4:
         frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    print(prediction)
+    print(prediction, " ", prediction_class)
     cv.imshow('Cam', frame)
     if cv.waitKey(1) == ord('q'):
         break
 cam.release()
 cv.destroyAllWindows()
-
-
-
-'''
-if not cam.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    ret, stream = cam.read()
-    if not ret:
-        print("Can't receive stream...")
-        break
-    cv.imshow('Camera 1', stream)
-    if cv.waitKey(1) == ord('q'):
-        break
-cam.release()
-cv.destroyAllWindows()
-'''
-
-'''
-detector = VideoObjectDetection()
-detector.setModelTypeAsYOLOv3()
-detector.setModelPath(os.path.join(path , "model.h5"))
-detector.loadModel()
-
-video_path = detector.detectObjectsFromVideo(camera_input=cam,
-                                output_file_path=os.path.join(path, "Cam")
-                                , frames_per_second=24, log_progress=True)
-print(video_path)
-'''
