1a0cbc7ac9439d53b373a94fe644fc8a8bf6db99
CresixU <40704964+CresixU@users.noreply.github.com>
2021-06-10 23:20:25 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,20 +1,17 @@
-import cv2
 import numpy as np
 import cv2 as cv
-import os
 from keras import models
-import tensorflow as tf
 
-model = models.load_model('main_model.h5')
+model = models.load_model('model4.h5')
 #cam
 cam_source = 'http://192.168.18.6:4747/video'
 cam = cv.VideoCapture(cam_source)
 
 #text cam
-font = cv2.FONT_HERSHEY_SIMPLEX
-org = (50,465)
+font = cv.FONT_HERSHEY_SIMPLEX
+org = (10,20)
 fontScale = 0.5
-color = (0,255,0) #BGR
+color = (0,255,0) # BGR
 thickness = 1
 
 if not cam.isOpened():
@@ -22,14 +19,15 @@ if not cam.isOpened():
     exit()
 while True:
     ret, frame = cam.read()
+    #frame = cv.resize(frame, (640, 640))
     if not ret:
         print("Can't receive stream...")
         break
-    image = cv.resize(frame, (64, 64)) # rozdzielczosc
-    image = image[:, :, [2, 1, 0]] # na RGB
-    image = image.reshape(1, 64, 64, 3) # dodatkowy wymiar
-    image = image.astype('float32') # typ na float32
-    image = image/255 # skalowanie
+    image = cv.resize(frame, (64, 64))
+    image = image[:, :, [2, 1, 0]]
+    image = image.reshape(1, 64, 64, 3)
+    image = image.astype('float32')
+    image = image/255
     prediction = model.predict(image)[0][0]
     pred = prediction*100
     pred = int(np.rint(pred))
@@ -39,13 +37,11 @@ while True:
     elif(int(prediction_class) == 1):
         pred_label = "Rotten"
     else:
-        pred_label = "Unknown"
+        pred_label = "None"
     pred = "Prediction: "+str(pred)+"% "+pred_label
     if prediction > 0.05:
         print(pred,prediction, " ", prediction_class)
-    if prediction < 0.4:
-        image = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    image = cv2.putText(frame, pred, org,font,fontScale,color,thickness,cv2.LINE_AA)
+    image = cv.putText(frame, pred, org, font, fontScale, color, thickness, cv.LINE_AA)
     cv.imshow('Cam', frame)
     if cv.waitKey(1) == ord('q'):
         break
