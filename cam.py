e96a081c41814d7c76bacdb86c5150cc0cb767e8
CresixU <40704964+CresixU@users.noreply.github.com>
2021-06-10 15:28:29 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -1,13 +1,22 @@
+import cv2
 import numpy as np
 import cv2 as cv
 import os
 from keras import models
 import tensorflow as tf
 
+model = models.load_model('main_model.h5')
+#cam
 cam_source = 'http://192.168.18.6:4747/video'
 cam = cv.VideoCapture(cam_source)
 
-model = models.load_model('model.h5')
+#text cam
+font = cv2.FONT_HERSHEY_SIMPLEX
+org = (50,465)
+fontScale = 0.5
+color = (0,255,0) #BGR
+thickness = 1
+
 if not cam.isOpened():
     print("Cannot open camera")
     exit()
@@ -22,10 +31,21 @@ while True:
     image = image.astype('float32') # typ na float32
     image = image/255 # skalowanie
     prediction = model.predict(image)[0][0]
+    pred = prediction*100
+    pred = int(np.rint(pred))
     prediction_class = model.predict(image).argmax(axis=-1)
+    if(int(prediction_class) == 0):
+        pred_label = "Healthy"
+    elif(int(prediction_class) == 1):
+        pred_label = "Rotten"
+    else:
+        pred_label = "Unknown"
+    pred = "Prediction: "+str(pred)+"% "+pred_label
+    if prediction > 0.05:
+        print(pred,prediction, " ", prediction_class)
     if prediction < 0.4:
-        frame = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    print(prediction, " ", prediction_class)
+        image = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    image = cv2.putText(frame, pred, org,font,fontScale,color,thickness,cv2.LINE_AA)
     cv.imshow('Cam', frame)
     if cv.waitKey(1) == ord('q'):
         break
