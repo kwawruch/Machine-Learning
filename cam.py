172527c10232f2814ebcdf85728e42af9beef8a9
CresixU <40704964+CresixU@users.noreply.github.com>
2021-06-14 20:48:04 +0200

    Update cam.py

--- cam.py
+++ cam.py
@@ -17,9 +17,9 @@ def retColor(percent):
             g=255
     return (b, g, r) #BGR
 
-model = models.load_model('model_bg.h5')
+model = models.load_model('model_bg_200ep_no_2_denses.h5')
 #cam
-cam_source = 'videos/3.mp4' #'http://192.168.18.6:4747/video'
+cam_source = 'videos/Ostatni.mp4'
 cam = cv.VideoCapture(cam_source)
 #text cam
 font = cv.FONT_HERSHEY_SIMPLEX
@@ -45,11 +45,8 @@ while True:
     image = image.reshape(1, 64, 64, 3)
     image = image.astype('float32')
     image = image/255
-    prediction = model.predict(image)[0][0]
-    p_t = str(model.predict(image))
-    pred = prediction*100
-    pred = int(np.rint(pred))
     prediction_class = model.predict(image).argmax(axis=-1)
+    p_class_id = int(prediction_class)
     if(int(prediction_class) == 0):
         pred_label = folders[0]
     elif(int(prediction_class) == 1):
@@ -58,12 +55,16 @@ while True:
         pred_label = folders[2]
     else:
         pred_label = "Unknown"
+    prediction = model.predict(image)[0][p_class_id]
+    p_t = str(model.predict(image))
+    pred = prediction*100
+    pred = int(np.rint(pred))
     pred_txt = "Prediction: "+str(pred)+"% "+pred_label
-    #if prediction > 0.05:
     print(pred_txt,prediction, " ", prediction_class," ",p_t)
+    image = cv.putText(frame, pred_txt, org, font, fontScale, (0, 0, 0), (thickness+1), cv.LINE_AA)
     image = cv.putText(frame, pred_txt, org, font, fontScale, retColor(pred), thickness, cv.LINE_AA)
     cv.imshow('Cam', frame)
     if cv.waitKey(1) == ord('q'):
         break
 cam.release()
-cv.destroyAllWindows()
+cv.destroyAllWindows()()
